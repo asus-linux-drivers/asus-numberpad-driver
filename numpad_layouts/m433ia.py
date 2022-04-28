68100af75b7d6bfd76d509bba63a0e066d7b68f3
Lukáš Drahník <ldrahnik@gmail.com>
2022-04-28 23:27:40 +0200

    Added configurable support of all levels of backlight

--- numpad_layouts/m433ia.py
+++ numpad_layouts/m433ia.py
@@ -8,6 +8,20 @@ cols = 5
 rows = 4
 top_offset = 0.3
 
+# Including off (first value of array) and full brightness (last value of array)
+backlight_levels = [
+    "0x00",
+    "0x01"
+]
+# Testing array on device with no levels of backglight
+#backlight_levels = [
+#    "0x00",
+#    "0x01",
+#    "0x00",
+#    "0x01",
+#    "0x00"
+#]
+
 keys = [
     [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPSLASH, EV_KEY.KEY_BACKSPACE],
     [EV_KEY.KEY_KP4, EV_KEY.KEY_KP5, EV_KEY.KEY_KP6, EV_KEY.KEY_KPASTERISK, EV_KEY.KEY_BACKSPACE],
