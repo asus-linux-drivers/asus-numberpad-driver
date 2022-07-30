fce9fbf0ef4ad621229111e1b7f647c2312541e8
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-07-30 14:25:13 +0200

    feat: 🎸 Add keyboard layout detection & format install script

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Checking if the script is runned as root (via sudo or other)
-if [[ $(id -u) != 0 ]]
-then
+if [[ $(id -u) != 0 ]]; then
     echo "Please run the installation script as root (using sudo for example)"
     exit 1
 fi
@@ -18,8 +17,7 @@ fi
 pip3 install numpy evdev
 
 # Checking if the pip3 is successfuly loaded
-if [[ $? != 0 ]]
-then
+if [[ $? != 0 ]]; then
     echo "pip3 is not loaded correctly. Make sure you have installed python3-pip package"
     exit 1
 fi
@@ -27,41 +25,38 @@ fi
 modprobe i2c-dev
 
 # Checking if the i2c-dev module is successfuly loaded
-if [[ $? != 0 ]]
-then
+if [[ $? != 0 ]]; then
     echo "i2c-dev module cannot be loaded correctly. Make sure you have installed i2c-tools package"
     exit 1
 fi
 
 interfaces=$(for i in $(i2cdetect -l | grep DesignWare | sed -r "s/^(i2c\-[0-9]+).*/\1/"); do echo $i; done)
-if [ -z "$interfaces" ]
-then
+if [ -z "$interfaces" ]; then
     echo "No i2c interface can be found. Make sure you have installed libevdev packages"
     exit 1
 fi
 
-touchpad_detected=false;
+touchpad_detected=false
 for i in $interfaces; do
-    echo -n "Testing interface $i : ";
+    echo -n "Testing interface $i : "
     number=$(echo -n $i | cut -d'-' -f2)
-	offTouchpadCmd="i2ctransfer -f -y $number w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad"
+    offTouchpadCmd="i2ctransfer -f -y $number w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad"
     i2c_test=$($offTouchpadCmd 2>&1)
-    if [ -z "$i2c_test" ]
-    then
+    if [ -z "$i2c_test" ]; then
         echo "sucess"
-        touchpad_detected=true;
+        touchpad_detected=true
         break
     else
         echo "failed"
     fi
-done;
+done
 
-if [ "$touchpad_detected" = false ] ; then
+if [ "$touchpad_detected" = false ]; then
     echo 'The detection was not successful. Touchpad not found.'
     exit 1
 fi
 
-if [[ -d numpad_layouts/__pycache__ ]] ; then
+if [[ -d numpad_layouts/__pycache__ ]]; then
     rm -rf numpad_layouts/__pycache__
 fi
 
@@ -69,35 +64,43 @@ echo
 echo "Select your model keypad layout:"
 PS3='Please enter your choice '
 options=($(ls numpad_layouts) "Quit")
-select selected_opt in "${options[@]}"
-do
-    if [ "$selected_opt" = "Quit" ]
-    then
+select selected_opt in "${options[@]}"; do
+    if [ "$selected_opt" = "Quit" ]; then
         exit 0
     fi
 
-    for option in $(ls numpad_layouts);
-    do
-        if [ "$option" = "$selected_opt" ] ; then
+    for option in $(ls numpad_layouts); do
+        if [ "$option" = "$selected_opt" ]; then
             model=${selected_opt::-3}
             break
         fi
     done
 
-    if [ -z "$model" ] ; then
+    if [ -z "$model" ]; then
         echo "invalid option $REPLY"
     else
         break
     fi
 done
 
-echo
-echo "What is your keyboard layout?"
-PS3='Please enter your choice [1-3]: '
-options=("Qwerty" "Azerty" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
+DETECTED_LAYOUT=$(setxkbmap -print | sed -nr 's#.*xkb_keycodes.*aliases\((.*)\).*#\1#p')
+
+case $DETECTED_LAYOUT in
+"qwerty")
+    percentage_key=6 # Number 5
+    ;;
+
+"azerty")
+    percentage_key=40 # Apostrophe key
+    ;;
+
+*)
+    echo "Keyboard layout not detected!"
+    echo "Please select your layout manually."
+    PS3='Please enter your choice [1-3]: '
+    options=("Qwerty" "Azerty" "Quit")
+    select opt in "${options[@]}"; do
+        case $opt in
         "Qwerty")
             percentage_key=6 # Number 5
             break
@@ -109,13 +112,14 @@ do
         "Quit")
             exit 0
             ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
-
+        *) echo "invalid option $REPLY" ;;
+        esac
+    done
+    ;;
+esac
 
 echo "Installing asus touchpad service to /etc/systemd/system/"
-cat asus_touchpad.service | LAYOUT=$model PERCENTAGE_KEY=$percentage_key envsubst '$LAYOUT $PERCENTAGE_KEY' > /etc/systemd/system/asus_touchpad_numpad.service
+cat asus_touchpad.service | LAYOUT=$model PERCENTAGE_KEY=$percentage_key envsubst '$LAYOUT $PERCENTAGE_KEY' >/etc/systemd/system/asus_touchpad_numpad.service
 
 mkdir -p /usr/share/asus_touchpad_numpad-driver/numpad_layouts
 mkdir -p /var/log/asus_touchpad_numpad-driver
@@ -126,8 +130,7 @@ echo "i2c-dev" | tee /etc/modules-load.d/i2c-dev.conf >/dev/null
 
 systemctl enable asus_touchpad_numpad
 
-if [[ $? != 0 ]]
-then
+if [[ $? != 0 ]]; then
     echo "Something went wrong when enabling the asus_touchpad_numpad.service"
     exit 1
 else
@@ -135,8 +138,7 @@ else
 fi
 
 systemctl restart asus_touchpad_numpad
-if [[ $? != 0 ]]
-then
+if [[ $? != 0 ]]; then
     echo "Something went wrong when enabling the asus_touchpad_numpad.service"
     exit 1
 else
