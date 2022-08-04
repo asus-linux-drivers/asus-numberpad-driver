af2c4c977c474605c7ba96c58648f40153934933
Lukáš Drahník <ldrahnik@gmail.com>
2022-08-04 23:42:41 +0200

    #21 gx701: physical POINTER buttons are outside of numpad area, do not ignore them when is numpad enabled

--- numpad_layouts/gx701.py
+++ numpad_layouts/gx701.py
@@ -3,6 +3,8 @@ from libevdev import EV_KEY
 # Subtract 0.3 (a third key) as the UX581L has about a third key space at the top
 top_offset = 0
 
+touchpad_physical_buttons_are_inside_numpad = False
+
 keys = [
     ["#", EV_KEY.KEY_KPSLASH, EV_KEY.KEY_KPASTERISK, EV_KEY.KEY_KPMINUS],
     [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPPLUS],
