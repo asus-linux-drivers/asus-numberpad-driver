dafba86256d8d8733dcdc94d784f68292510bc07
Lukáš Drahník <ldrahnik@gmail.com>
2022-08-10 03:57:46 +0200

    Added implementation of keys_ignore_offset because in model gx551 is Numlock on the top left and should be pressed do not care about padding

--- numpad_layouts/gx551.py
+++ numpad_layouts/gx551.py
@@ -17,3 +17,7 @@ keys = [
     [EV_KEY.KEY_KP1, EV_KEY.KEY_KP2, EV_KEY.KEY_KP3, EV_KEY.KEY_KPENTER],
     [EV_KEY.KEY_KP0, EV_KEY.KEY_KP0, EV_KEY.KEY_KPDOT, EV_KEY.KEY_KPENTER]
 ]
+
+keys_ignore_offset = [
+    [0, 0]
+]
