eff4970d790fb3ba34a55d5acb1f733555ae5539
Lukáš Drahník <ldrahnik@gmail.com>
2022-12-09 12:24:33 +0100

    #69 fixed vertical layout ux581l

--- numpad_layouts/ux581l.py
+++ numpad_layouts/ux581l.py
@@ -1,10 +1,25 @@
 from libevdev import EV_KEY
 
-# Subtract 0.3 (a third key) as the UX581L has about a third key space at the top
-top_offset = 0.3
+top_left_icon_width = 200
+top_left_icon_height = 200
 
-# As the UX581L has a "vertical numpad"
-# Compared to the more "horizontal" version of the UM433DA (for example)
+top_right_icon_width = 200
+top_right_icon_height = 200
+
+top_offset = 200
+right_offset = 80
+left_offset = 80
+bottom_offset = 80
+
+backlight_levels = [
+    "0x41",
+    "0x42",
+    "0x43",
+    "0x44",
+    "0x45",
+    "0x46"
+]
+
 keys = [
     [EV_KEY.KEY_KPEQUAL, "%", EV_KEY.KEY_BACKSPACE, EV_KEY.KEY_BACKSPACE],
     [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPSLASH],
