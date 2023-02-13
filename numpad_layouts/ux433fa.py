1d0340c201f4c4a1079251e549c68b192499f858
Lukáš Drahník <ldrahnik@gmail.com>
2023-02-13 11:57:37 +0100

    Attempt to fix layout ux433fa

--- numpad_layouts/ux433fa.py
+++ numpad_layouts/ux433fa.py
@@ -1,6 +1,12 @@
 from libevdev import EV_KEY
 
-top_offset = 0.10
+top_right_icon_width = 150
+top_right_icon_height = 250
+
+top_offset = 80
+right_offset = 40
+left_offset = 40
+bottom_offset = 80
 
 keys = [
     [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPSLASH, EV_KEY.KEY_BACKSPACE],
