95ef83a1957b391084248c1bf0cdca14893d4863
Lukáš Drahník <ldrahnik@gmail.com>
2023-02-19 13:31:52 +0100

    To un-install script added possibility do not remove conf files when differ from defaul ones

--- uninstall.sh
+++ uninstall.sh
@@ -27,11 +27,82 @@ then
 	exit 1
 fi
 
-rm -rf /usr/share/asus_touchpad_numpad-driver/
-if [[ $? != 0 ]]
+
+if [[ -d /usr/share/asus_touchpad_numpad-driver/numpad_layouts/__pycache__ ]]; then
+    rm -rf /usr/share/asus_touchpad_numpad-driver/numpad_layouts/__pycache__
+fi
+
+# for `rm` exclude !(xy)
+shopt -s extglob
+
+NUMPAD_LAYOUTS_DIR="/usr/share/asus_touchpad_numpad-driver/numpad_layouts/"
+
+NUMPAD_LAYOUTS_DIR_DIFF=""
+if test -d "$NUMPAD_LAYOUTS_DIR"; then
+    NUMPAD_LAYOUTS_DIR_DIFF=$(diff numpad_layouts $NUMPAD_LAYOUTS_DIR)
+fi
+
+if [ "$NUMPAD_LAYOUTS_DIR_DIFF" != "" ]
 then
-	echo "/usr/share/asus_touchpad_numpad-driver/ cannot be removed correctly..."
-	exit 1
+    read -r -p "Installed numpad layouts contain modifications compared to the default ones. Do you want remove them [y/N]" response
+    case "$response" in [yY][eE][sS]|[yY])
+		rm -rf /usr/share/asus_touchpad_numpad-driver/!(asus_touchpad_numpad_dev)
+		if [[ $? != 0 ]]
+		then
+			echo "/usr/share/asus_touchpad_numpad-driver/ cannot be removed correctly..."
+			exit 1
+		fi
+        ;;
+    *)
+		rm -rf /usr/share/asus_touchpad_numpad-driver/!(numpad_layouts|asus_touchpad_numpad_dev)
+		if [[ $? != 0 ]]
+		then
+			echo "/usr/share/asus_touchpad_numpad-driver/ cannot be removed correctly..."
+			exit 1
+		fi
+		echo "Numpad layouts in /usr/share/asus_touchpad_numpad-driver/conf/ have not been removed and remain in system:"
+        ls /usr/share/asus_touchpad_numpad-driver/numpad_layouts
+        ;;
+    esac
+else
+	rm -rf /usr/share/asus_touchpad_numpad-driver/!(asus_touchpad_numpad_dev)
+	if [[ $? != 0 ]]
+	then
+		echo "/usr/share/asus_touchpad_numpad-driver/ cannot be removed correctly..."
+		exit 1
+	fi
+fi
+
+CONF_FILE="/usr/share/asus_touchpad_numpad-driver/asus_touchpad_numpad_dev"
+
+CONFIG_FILE_DIFF=""
+if test -f "$CONF_FILE"; then
+	CONFIG_FILE_DIFF=$(diff <(grep -v '^#' asus_touchpad_numpad_dev) <(grep -v '^#' $CONF_FILE))
+fi
+
+if [ "$CONFIG_FILE_DIFF" != "" ]
+then
+    read -r -p "Config file contain modifications compared to the default one. Do you want remove config file [y/N]" response
+    case "$response" in [yY][eE][sS]|[yY])
+		rm -f /usr/share/asus_touchpad_numpad-driver/asus_touchpad_numpad_dev
+		if [[ $? != 0 ]]
+		then
+			echo "/usr/share/asus_touchpad_numpad-driver/asus_touchpad_numpad_dev cannot be removed correctly..."
+			exit 1
+		fi
+        ;;
+    *)
+		echo "Config file have not been removed and remain in system:"
+		echo "/usr/share/asus_touchpad_numpad-driver/asus_touchpad_numpad_dev"
+        ;;
+    esac
+else
+	rm -f /usr/share/asus_touchpad_numpad-driver/asus_touchpad_numpad_dev
+	if [[ $? != 0 ]]
+	then
+		echo "/usr/share/asus_touchpad_numpad-driver/asus_touchpad_numpad_dev cannot be removed correctly..."
+		exit 1
+	fi
 fi
 
 rm -rf /var/log/asus_touchpad_numpad-driver
@@ -48,5 +119,5 @@ then
 	exit 1
 fi
 
-echo "Asus touchpad python driver uninstalled"
+echo "Uninstall finished"
 exit 0
