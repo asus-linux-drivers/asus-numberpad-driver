acd50fe1d1e5c9c2a2b73a70ac93ba564e400386
Lukáš Drahník <ldrahnik@gmail.com>
2023-06-29 15:02:52 +0200

    #125 minimalize number of keys even with KP names because them might be overbind aswell (Turkish keyboard)

--- numpad_layouts/gx551.py
+++ numpad_layouts/gx551.py
@@ -4,11 +4,11 @@ left_offset = 20
 right_offset = 20
 
 keys = [
-    [EV_KEY.KEY_NUMLOCK, EV_KEY.KEY_KPSLASH, EV_KEY.KEY_KPASTERISK, EV_KEY.KEY_KPMINUS],
-    [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPPLUS],
-    [EV_KEY.KEY_KP4, EV_KEY.KEY_KP5, EV_KEY.KEY_KP6, EV_KEY.KEY_KPPLUS],
-    [EV_KEY.KEY_KP1, EV_KEY.KEY_KP2, EV_KEY.KEY_KP3, EV_KEY.KEY_KPENTER],
-    [EV_KEY.KEY_KP0, EV_KEY.KEY_KP0, EV_KEY.KEY_KPDOT, EV_KEY.KEY_KPENTER]
+    [EV_KEY.KEY_NUMLOCK, "/", "*", "-"],
+    ["7", "8", "9", "+"],
+    ["4", "5", "6", "+"],
+    ["1", "2", "3", EV_KEY.KEY_KPENTER],
+    ["0", "0", ".", EV_KEY.KEY_KPENTER]
 ]
 
 keys_ignore_offset = [
