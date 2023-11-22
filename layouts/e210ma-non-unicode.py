264a25140d291c39807de8b1b3b5177e7b6d1507
Lukáš Drahník <ldrahnik@gmail.com>
2023-11-22 19:09:08 +0100

    Precreated non-unicode layouts with used Shift+3/5 shortcut for percent, hashtag char

---
+++ layouts/e210ma-non-unicode.py
@@ -0,0 +1,19 @@
+from libevdev import EV_KEY
+
+top_right_icon_width = 250
+top_right_icon_height = 250
+
+top_left_icon_width = 250
+top_left_icon_height = 250
+
+top_offset = 240
+right_offset = 200
+left_offset = 200
+bottom_offset = 40
+
+keys = [
+    [EV_KEY.KEY_KP7, EV_KEY.KEY_KP8, EV_KEY.KEY_KP9, EV_KEY.KEY_KPSLASH, EV_KEY.KEY_BACKSPACE],
+    [EV_KEY.KEY_KP4, EV_KEY.KEY_KP5, EV_KEY.KEY_KP6, EV_KEY.KEY_KPASTERISK, EV_KEY.KEY_BACKSPACE],
+    [EV_KEY.KEY_KP1, EV_KEY.KEY_KP2, EV_KEY.KEY_KP3, EV_KEY.KEY_KPMINUS, [EV_KEY.KEY_LEFTSHIFT, EV_KEY.KEY_KP5]],
+    [EV_KEY.KEY_KP0, EV_KEY.KEY_KPDOT, EV_KEY.KEY_KPENTER, EV_KEY.KEY_KPPLUS, EV_KEY.KEY_KPEQUAL]
+]
