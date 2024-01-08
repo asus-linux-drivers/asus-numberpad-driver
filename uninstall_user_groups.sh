dfd6edb0f9ff2d7c280014d841ea501f27caebf0
ldrahnik@gmail.com <ldrahnik@gmail.com>
2024-01-08 13:23:25 +0100

    #155 added udev rule for setting up /dev/i2c-x to i2c group

--- uninstall_user_groups.sh
+++ uninstall_user_groups.sh
@@ -8,6 +8,11 @@ if [[ $? != 0 ]]; then
     echo "Something went wrong when removing the uinput udev rule"
 fi
 
+sudo rm -f /usr/lib/udev/rules.d/99-asus-numberpad-driver-i2c-dev.rules
+if [[ $? != 0 ]]; then
+    echo "Something went wrong when removing the i2c-dev udev rule"
+fi
+
 sudo rm -f /etc/modules-load.d/uinput-asus-numberpad-driver.conf
 if [[ $? != 0 ]]; then
     echo "Something went wrong when removing the uinput conf"
