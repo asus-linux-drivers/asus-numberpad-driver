125207bbf9ff64b46c3ce28204d6827bc83155da
ldrahnik@gmail.com <ldrahnik@gmail.com>
2024-01-10 16:32:04 +0100

    Bump to v4.2.2

--- install_user_groups.sh
+++ install_user_groups.sh
@@ -25,11 +25,20 @@ else
     echo "uinput module loaded"
 fi
 
-sudo chown :uinput /dev/uinput
+sudo modprobe i2c-dev
+
+# check if the i2c-dev module is successfully loaded
+if [[ $? != 0 ]]; then
+    echo "i2c-dev module cannot be loaded. Make sure you have installed i2c-tools package"
+    exit 1
+else
+    echo "i2c-dev module loaded"
+fi
 
 echo 'KERNEL=="uinput", GROUP="uinput", MODE="0660"' | sudo tee /usr/lib/udev/rules.d/99-asus-numberpad-driver-uinput.rules >/dev/null
 echo 'uinput' | sudo tee /etc/modules-load.d/uinput-asus-numberpad-driver.conf >/dev/null
 echo 'SUBSYSTEM=="i2c-dev", GROUP="i2c", MODE="0660"' | sudo tee /usr/lib/udev/rules.d/99-asus-numberpad-driver-i2c-dev.rules >/dev/null
+echo "i2c-dev" | sudo tee /etc/modules-load.d/i2c-dev-asus-numberpad-driver.conf >/dev/null
 
 if [[ $? != 0 ]]; then
     echo "Something went wrong when adding uinput module to auto loaded modules"
@@ -38,7 +47,7 @@ else
     echo "uinput module added to auto loaded modules"
 fi
 
-sudo udevadm control --reload-rules && sudo udevadm trigger --sysname-match=uinput
+sudo udevadm control --reload-rules && sudo udevadm trigger --sysname-match=uinput && sudo udevadm trigger --attr-match=subsystem=i2c-dev
 
 if [[ $? != 0 ]]; then
     echo "Something went wrong when reloading or triggering uinput udev rules"
