a450dde9e85568b3bcdc97bb046435da7f3ba2bc
ldrahnik@gmail.com <ldrahnik@gmail.com>
2024-01-10 13:43:44 +0100

    WIP

--- install.sh
+++ install.sh
@@ -29,10 +29,23 @@ LOGS_INSTALL_LOG_FILE_PATH="$LOGS_DIR_PATH/$LOGS_INSTALL_LOG_FILE_NAME"
         fi
     elif [[ $(sudo pacman -h 2>/dev/null) ]]; then
         # arch does not have header packages (python3-dev), headers are shipped with base? python package should contains almost latest version python3.*
-        sudo pacman --noconfirm --needed -S ibus libevdev curl xorg-xinput i2c-tools python python-libevdev python-numpy python-pyinotify python-xlib libxml2 python3-smbus2
+        sudo pacman --noconfirm --needed -S base-devel git ibus libevdev curl xorg-xinput i2c-tools python python-libevdev python-numpy python-pyinotify python-xlib libxml2
 
         if [ "$(printf '%s\n' "$PYTHON_REQUIRED_VERSION_FOR_LIB_PYASYNCORE" "$PYTHON_VERSION" | sort -V | head -n1)" = "$PYTHON_REQUIRED_VERSION_FOR_LIB_PYASYNCORE" ]; then 
             sudo pacman --noconfirm --needed -S python3-pyasyncore
+        fi
+
+        # because package python3-smbus2 is available only on AUR
+        #
+        # https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/156
+        if [[ $(yay)]]; then
+            yay -S python-smbus2
+        else
+            git clone https://aur.archlinux.org/python-smbus2.git ~/python-smbus2
+            cd ~/python-smbus2
+            # -i     it calls pacman -U <package>
+            # -s     instructs pacman to resolve dependencies and install missing packages
+            makepkg -si
         fi
     elif [[ $(sudo dnf help 2>/dev/null) ]]; then
         sudo dnf -y install ibus libevdev curl xinput i2c-tools python3-devel python3-libevdev python3-numpy python3-inotify python3-xlib libxml2 python3-smbus2
