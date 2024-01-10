12dbd8ebc4c806b73ef322ff7407099ac23dbcb5
ldrahnik@gmail.com <ldrahnik@gmail.com>
2024-01-10 18:00:14 +0100

    WIP

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ LOGS_INSTALL_LOG_FILE_PATH="$LOGS_DIR_PATH/$LOGS_INSTALL_LOG_FILE_NAME"
         sudo apt-get -y install ibus libevdev2 curl xinput i2c-tools python3-dev python3-virtualenv libxml2-utils
     elif [[ $(sudo pacman -h 2>/dev/null) ]]; then
         # arch does not have header packages (python3-dev), headers are shipped with base? python package should contains almost latest version python3.*
-        sudo pacman --noconfirm --needed -S ibus libevdev curl xorg-xinput i2c-tools python python-pip python-virtualenv libxml2
+        sudo pacman --noconfirm --needed -S ibus libevdev curl xorg-xinput i2c-tools python python-virtualenv libxml2
     elif [[ $(sudo dnf help 2>/dev/null) ]]; then
         sudo dnf -y install ibus libevdev curl xinput i2c-tools python3-devel python3-virtualenv libxml2
     elif [[ $(sudo yum help 2>/dev/null) ]]; then
