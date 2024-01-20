13a30f86ae09e898d2868ac76ca1f31e1820e477
Lukáš Drahník <ldrahnik@gmail.com>
2024-01-20 12:17:12 +0100

    #161 implemented different i2c register address for ASUF laptops ASUF1416, ASUF1205, ASUF1204

--- tests/test_brightness.py
+++ tests/test_brightness.py
@@ -7,6 +7,7 @@ from time import sleep
 
 keyboard_detected = 0
 touchpad_detected = 0
+device_addr = 0x00
 
 with open('/proc/bus/input/devices', 'r') as f:
 
@@ -19,7 +20,14 @@ with open('/proc/bus/input/devices', 'r') as f:
 		# https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/110
 		if (touchpad_detected == 0 and ("Name=\"ASUE" in line or "Name=\"ELAN" in line or "Name=\"ASUP" in line or "Name=\"ASUF" in line) and "Touchpad" in line) or \
 			(("Name=\"ASUE" in line or "Name=\"ELAN" in line or "Name=\"ASUP" in line or "Name=\"ASUF" in line) and ("1406" in line or "4F3:3101" in line) and "Touchpad" in line):
+
 			touchpad_detected = 1
+
+			# https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/161
+			if ("ASUF1416" in line or "ASUF1205" in line or "ASUF1204" in line):
+				device_addr = 0x38
+			else:
+				device_addr = 0x15
 
 		if touchpad_detected == 1:
 			if "S: " in line:
@@ -32,7 +40,7 @@ with open('/proc/bus/input/devices', 'r') as f:
 
 		# Look for the keyboard (numlock) # AT Translated Set OR Asus Keyboard
 		if keyboard_detected == 0 and ("Name=\"AT Translated Set 2 keyboard" in line or (("Name=\"ASUE" in line or "Name=\"Asus" in line or "Name=\"ASUP" in line or "Name=\"ASUF" in line) and "Keyboard" in line)):
-            keyboard_detected = 1
+		  keyboard_detected = 1
 
 		if keyboard_detected == 1:
 			if "H: " in line:
@@ -40,29 +48,29 @@ with open('/proc/bus/input/devices', 'r') as f:
 				keyboard = keyboard.split(" ")[0]
 				keyboard_detected = 2
 
-        # Do not stop looking if touchpad and keyboard have been found 
+        # Do not stop looking if touchpad and keyboard have been found
         # because more drivers can be installed
         # https://github.com/mohamed-badaoui/asus-touchpad-numpad-driver/issues/87
         # https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/95
 		# https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/110
 		#if keyboard_detected == 2 and touchpad_detected == 2:
 		#	break
- 
+
 for i in range(2, 255):
 	value = str(hex(i))
 	valueLowestBrightness = "0x41"
-	cmdOn = "i2ctransfer -f -y " + device_id + " w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 " + "0x01" + " 0xad"
+	cmdOn = "i2ctransfer -f -y " + device_id + " w13@" + str(device_addr) + " 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 " + "0x01" + " 0xad"
 	print("0x01")
 	os.system(cmdOn)
 	sleep(1)
-	cmdLowestBrightness = "i2ctransfer -f -y " + device_id + " w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 " + valueLowestBrightness + " 0xad"
+	cmdLowestBrightness = "i2ctransfer -f -y " + device_id + " w13@" + str(device_addr) + " 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 " + valueLowestBrightness + " 0xad"
 	print(valueLowestBrightness)
 	os.system(cmdLowestBrightness)
 	sleep(1)
 	print("Tested value of registr: " + str(i) + " (hex: " + value + ")")
-	cmdNewValue = "i2ctransfer -f -y " + device_id + " w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 " + value + " 0xad"
+	cmdNewValue = "i2ctransfer -f -y " + device_id + " w13@" + str(device_addr) + " 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 " + value + " 0xad"
 	os.system(cmdNewValue)
 	input("Press Enter to continue...")
- 
-cmdoff = "i2ctransfer -f -y " + device_id + " w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad"
+
+cmdoff = "i2ctransfer -f -y " + device_id + " w13@" + str(device_addr) + " 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad"
 os.system(cmdoff)
