79d25295c8421530eea87d7cd5453eb58d5589d1
Lukáš Drahník <ldrahnik@gmail.com>
2024-04-23 13:28:06 +0200

    WIP

--- install_layout_select.sh
+++ install_layout_select.sh
@@ -9,8 +9,8 @@ if [ -z "$LAYOUT_NAME" ]; then
     echo "NumberPad layout"
     echo
     echo "2 variants of NumberPad layouts are predefined for each laptop:"
-    echo " - Standard. All keys are sent directly except not found (modifiers are supported), which is why the unicode variant exists"
-    echo " - The unicode variant sends not number characters (%, #, ., -, +, *, /) using the unicode shortcut (usually Ctrl+Shift+U+<0-F>). This layout is the most resistant to missing bindings of keys but sends multiple keys instead of just one, unnecessarily heavy if you do not need it."
+    echo " - Standard. For all fields of NumberPad layout will try to found appropriate keys that are in success sent. Key modifiers are supported. Is required to have bound used special characters (%, *, #, ., -, +, /) with usual keysyms (percent, asterisk, numbersign, period, minus, plus, slash). When is appropriate key not found than field will be sent using the unicode shortcut (usually Ctrl+Shift+U+<0-F>)."
+    echo " - The unicode variant sends special characters (%, #, ., -, +, *, /) using the unicode shortcut (usually Ctrl+Shift+U+<0-F>). This layout allows to have not bound special characters with usual keysyms like percent, asterisk, plus, minus, slash, numbersign. Unnecessarily heavy (number of events sent) if you do not need it."
     echo
     echo "Select layout (photos of NumberPad layouts can be found here https://github.com/asus-linux-drivers/asus-numberpad-driver#layouts):"
     echo
