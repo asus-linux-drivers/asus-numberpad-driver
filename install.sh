3c84b2ea31ca32224009b7d5e1c69f29c4d53bc3
Lukáš Drahník <ldrahnik@gmail.com>
2024-06-02 10:15:09 +0200

    #182 init portage support (credits @v0llk)

--- install.sh
+++ install.sh
@@ -36,7 +36,10 @@ LOGS_INSTALL_LOG_FILE_PATH="$LOGS_DIR_PATH/$LOGS_INSTALL_LOG_FILE_NAME"
         sudo zypper --non-interactive install ibus libevdev2 curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc
     elif [[ $(sudo xbps-install --help 2>/dev/null) ]]; then
         PACKAGE_MANAGER="xbps-install"
-        sudo xbps-install -Suy ibus-devel libevdev-devel curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc 
+        sudo xbps-install -Suy ibus-devel libevdev-devel curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc
+    elif [[ $(sudo emerge -h 2>/dev/null) ]]; then
+        PACKAGE_MANAGER="portage"
+        sudo emerge app-i18n/ibus dev-libs/libevdev net-misc/curl x11-apps/xinput sys-apps/i2c-tools dev-lang/python dev-python/virtualenv dev-libs/libxml2 x11-libs/libxkbcommon sys-devel/gcc
     else
         echo "Not detected package manager. Driver may not work properly because required packages have not been installed. Please create an issue (https://github.com/asus-linux-drivers/asus-numberpad-driver/issues)."
     fi
