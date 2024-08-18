83771f85b692959aee4d6b30952725d4d11c2016
Imran Khan <encrustace@gmail.com>
2024-08-18 22:28:21 +0530

    Fixed gnome calculator toggle

--- scripts/gnome_calculator_toggle.sh
+++ scripts/gnome_calculator_toggle.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ $(flatpak list | grep org.gnome.Calculator) ]]; then
-  if [[ $(pgrep -lfx org.gnome.Calculator) ]]; then
-  	flatpak run org.gnome.Calculator &>/dev/null &
+  if [[ $(pgrep -lfx gnome-calculator) ]]; then
+    killall gnome-calculator &>/dev/null &
   else
-    flatpak kill org.gnome.Calculator &>/dev/null &
+    flatpak run org.gnome.Calculator &>/dev/null &
   fi
 else
   if [[ $(pgrep -lfx gnome-calculator) ]]; then
