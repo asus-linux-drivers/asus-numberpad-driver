ee818d641c80609f34d25bebbb5983114187426a
Lukáš Drahník <ldrahnik@gmail.com>
2024-08-18 19:28:03 +0200

    #195 added support for gnome-calculator installed via flatpak + fixed io.elementary.calculator (credits @encrustace)

--- scripts/gnome_calculator_toggle.sh
+++ scripts/gnome_calculator_toggle.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ $(pgrep -lf gnome-calculator) ]]; then
-	killall gnome-calculator &>/dev/null &
+if [[ $(pgrep -xlf gnome-calculator &>/dev/null ; echo $?) != 0 ]]; then
+  if [[ $(flatpak list | grep org.gnome.Calculator) ]]; then
+    flatpak run org.gnome.Calculator &>/dev/null &
+  else
+    gnome-calculator &>/dev/null &
+  fi
 else
-	gnome-calculator &>/dev/null &
+	killall gnome-calculator &>/dev/null &
 fi
