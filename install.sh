e9f8dcaf380384d959df4a2568a82b1d6996c8c0
Lukáš Drahník <ldrahnik@gmail.com>
2024-09-22 14:31:47 +0200

    #204 simplified detection of package managers

--- install.sh
+++ install.sh
@@ -17,29 +17,29 @@ LOGS_INSTALL_LOG_FILE_PATH="$LOGS_DIR_PATH/$LOGS_INSTALL_LOG_FILE_NAME"
 
 {
     # pip pywayland requires gcc
-    if [[ $(sudo apt-get install 2>/dev/null) ]]; then
+    if [[ $(command -v apt-get 2>/dev/null) ]]; then
         PACKAGE_MANAGER="apt"
         sudo apt-get -y install ibus libevdev2 curl xinput i2c-tools python3-dev python3-virtualenv libxml2-utils libxkbcommon-dev gcc
-    elif [[ $(sudo pacman -h 2>/dev/null) ]]; then
+    elif [[ $(command -v pacman 2>/dev/null) ]]; then
         PACKAGE_MANAGER="pacman"
         # arch does not have header packages (python3-dev), headers are shipped with base? python package should contains almost latest version python3.*
         sudo pacman --noconfirm --needed -S ibus libevdev curl xorg-xinput i2c-tools python python-virtualenv libxml2 libxkbcommon gcc
-    elif [ $(sudo dnf help 2>/dev/null) ] || [ $(sudo dnf --help 2>/dev/null) ]; then
+    elif [[ $(command -v dnf 2>/dev/null) ]]; then
         # dnf5 has --help
         # https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/204
         PACKAGE_MANAGER="dnf"
         sudo dnf -y install ibus libevdev curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc
-    elif [[ $(sudo yum help 2>/dev/null) ]]; then
+    elif [[ $(command -v yum 2>/dev/null) ]]; then
         PACKAGE_MANAGER="yum"
         # yum was replaced with newer dnf above
         sudo yum --y install ibus libevdev curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc
-    elif [[ $(sudo zypper help 2>/dev/null) ]]; then
+    elif [[ $(command -v zypper 2>/dev/null) ]]; then
         PACKAGE_MANAGER="zypper"
         sudo zypper --non-interactive install ibus libevdev2 curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc
-    elif [[ $(sudo xbps-install --help 2>/dev/null) ]]; then
+    elif [[ $(command -v xbps-install 2>/dev/null) ]]; then
         PACKAGE_MANAGER="xbps-install"
         sudo xbps-install -Suy ibus-devel libevdev-devel curl xinput i2c-tools python3-devel python3-virtualenv libxml2 libxkbcommon-devel gcc
-    elif [[ $(sudo emerge -h 2>/dev/null) ]]; then
+    elif [[ $(command -v emerge 2>/dev/null) ]]; then
         PACKAGE_MANAGER="portage"
         sudo emerge app-i18n/ibus dev-libs/libevdev net-misc/curl x11-apps/xinput sys-apps/i2c-tools dev-lang/python dev-python/virtualenv dev-libs/libxml2 x11-libs/libxkbcommon sys-devel/gcc
     else
