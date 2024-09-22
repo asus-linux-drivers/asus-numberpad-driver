f2da5b16013c65fd0eec47a3ae8a3a1c2addcd82
Lukáš Drahník <ldrahnik@gmail.com>
2024-09-22 14:32:13 +0200

    #204 removed which as unnecessary dependency

--- install.sh
+++ install.sh
@@ -102,7 +102,7 @@ LOGS_INSTALL_LOG_FILE_PATH="$LOGS_DIR_PATH/$LOGS_INSTALL_LOG_FILE_NAME"
     echo
 
     # create Python3 virtual environment
-    virtualenv --python=$(which python3) /usr/share/asus-numberpad-driver/.env
+    virtualenv --python=$(command -v python3) /usr/share/asus-numberpad-driver/.env
     source /usr/share/asus-numberpad-driver/.env/bin/activate
     pip3 install --upgrade pip
     pip3 install --upgrade setuptools
