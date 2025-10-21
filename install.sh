9a973b1ef06029486d99daafad3e45356ce20a9b
g3nsvrv <11720535+g3nsvrv@users.noreply.github.com>
2025-10-21 17:36:26 -0300

    fixing qdbus repo check on dnf

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ detect_package_manager() {
         WAYLAND_PKG="wayland-devel"
         UPDATE_CMD="sudo dnf -y update"
         INSTALL_CMD="sudo dnf -y install"
-        QDBUS_QUERY="dnf provides /usr/bin/qdbus 2>/dev/null | grep -oP '(qt\\d+-qttools-[^-]+-[^-]+)' | head -n1"
+        QDBUS_QUERY="dnf provides /usr/bin/qdbus 2>/dev/null | awk '/qt[56]-qttools.*'$(uname -m)'/ {print \$1; exit} /qt-.*'$(uname -m)'/ {print \$1; exit}' | head -n1"
         QDBUS_FALLBACK="qt6-qttools"
     elif command -v yum >/dev/null 2>&1; then
         PACKAGE_MANAGER="yum"
