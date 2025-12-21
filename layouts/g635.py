38198d181d1381df46d3e98814cb8e81581cc26f
Lukas Drahnik <ldrahnik@gmail.com>
2025-12-21 13:21:09 +0100

    Added layout for Asus Rog Strix Scar G635LW

---
+++ layouts/g635.py
@@ -0,0 +1,16 @@
+from libevdev import EV_KEY
+
+top_right_icon_width = 600
+top_right_icon_height = 300
+
+top_offset = 320
+right_offset = 80
+left_offset = 80
+bottom_offset = 200
+
+keys = [
+    ["7", "8", "9", "slash", "BackSpace"],
+    ["4", "5", "6", "asterisk", "BackSpace"],
+    ["1", "2", "3", "minus", "Return"],
+    ["0", "0", "period", "plus", "Return"]
+]
