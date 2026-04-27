21826947f7a530ad961a2f73099afd7c19f08727
Lukas Drahnik <ldrahnik@gmail.com>
2026-04-27 12:08:48 +0200

    Enhanced device check about users feedback whether was the DialPad light up

--- install_device_check.sh
+++ install_device_check.sh
@@ -2,10 +2,29 @@
 
 source non_sudo_check.sh
 
+send_test_activation() {
+    local number=$1
+    local addr=$2
+
+    sudo /usr/sbin/i2ctransfer -f -y "$number" w13@0x"$addr" \
+        0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x01 0xad \
+    && sudo /usr/sbin/i2ctransfer -f -y "$number" w13@0x"$addr" \
+        0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x60 0xad
+}
+
+send_test_deactivation() {
+    local number=$1
+    local addr=$2
+
+    sudo /usr/sbin/i2ctransfer -f -y "$number" w13@0x"$addr" \
+    0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad
+}
+
 # i2cdetect is /usr/sbin/i2cdetect and some distributions do not add sbin to $PATH (https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/154)
 # https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/218
-if [[ $(type /usr/sbin/i2cdetect 2>/dev/null) ]] && [[ $(type /usr/sbin/i2ctransfer 2>/dev/null) ]]; then
-    INTERFACES=$(for i in $(sudo /usr/sbin/i2cdetect -l | grep DesignWare | sed -E "s/^(i2c\-[0-9]+).*/\1/"); do echo $i; done)
+if command -v /usr/sbin/i2cdetect >/dev/null 2>&1 && command -v /usr/sbin/i2ctransfer >/dev/null 2>&1; then
+
+    INTERFACES=$(grep -A5 -i touchpad /proc/bus/input/devices | grep -o 'i2c-[0-9]\+' | sort -u)
 
     if [ -z "$INTERFACES" ]; then
         echo "No i2c interface can be found. Make sure you have installed libevdev packages"
@@ -18,31 +37,60 @@ if [[ $(type /usr/sbin/i2cdetect 2>/dev/null) ]] && [[ $(type /usr/sbin/i2ctrans
         echo -n "Testing interface $INDEX: "
 
         NUMBER=$(echo -n $INDEX | cut -d'-' -f2)
-        NUMBERPAD_OFF_CMD="sudo /usr/sbin/i2ctransfer -f -y $NUMBER w13@0x15 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad"
-        I2C_TEST_15=$($NUMBERPAD_OFF_CMD 2>&1)
 
-        # https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/161
-        NUMBERPAD_OFF_CMD="sudo /usr/sbin/i2ctransfer -f -y $NUMBER w13@0x38 0x05 0x00 0x3d 0x03 0x06 0x00 0x07 0x00 0x0d 0x14 0x03 0x00 0xad"
-        I2C_TEST_38=$($NUMBERPAD_OFF_CMD 2>&1)
+        second_try_addr=
 
-        if [ -z "$I2C_TEST_15" ]; then
-            echo "success (adr 0x15)"
-            DEVICE_ADDRESS=15
-            TOUCHPAD_WITH_NUMBERPAD_DETECTED=true
-            break
-        elif [ -z "$I2C_TEST_38" ]; then
-            echo "success (adr 0x38)"
-            DEVICE_ADDRESS=38
-            TOUCHPAD_WITH_NUMBERPAD_DETECTED=true
-            break
+        # OFF test 0x15 
+        if send_test_deactivation "$NUMBER" 15 >/dev/null 2>&1; then
+            first_try_addr=15
+
+            # OFF test 0x38
+            if send_test_deactivation "$NUMBER" 38 >/dev/null 2>&1; then
+                second_try_addr=38
+            fi
+        # OFF test 0x38
+        elif send_test_deactivation "$NUMBER" 38 >/dev/null 2>&1; then
+            first_try_addr=38
         else
             echo "failed"
+            continue
         fi
+
+        send_test_activation "$NUMBER" "$first_try_addr"
+        echo
+        read -p "Did NumberPad light up? (0x$first_try_addr) [y/N]: " OK_FIRST
+
+        if [[ "$OK_FIRST" =~ ^[Yy]$ ]]; then
+            DEVICE_ADDRESS=$first_try_addr
+            TOUCHPAD_WITH_NUMBERPAD_DETECTED=true
+            send_test_deactivation "$NUMBER" "$first_try_addr"
+            break
+        fi
+
+        if [ -n "$second_try_addr" ]; then
+
+            send_test_activation "$NUMBER" "$second_try_addr"
+            read -p "Did NumberPad light up? (0x$second_try_addr) [y/N]: " OK_SECOND
+
+            if [[ "$OK_SECOND" =~ ^[Yy]$ ]]; then
+                DEVICE_ADDRESS=$second_try_addr
+                TOUCHPAD_WITH_NUMBERPAD_DETECTED=true
+                send_test_deactivation "$NUMBER" "$second_try_addr"
+                break
+            fi
+        fi
+
+        echo "failed"
     done
 
     if [ -z "$TOUCHPAD_WITH_NUMBERPAD_DETECTED" ]; then
-        echo "The detection was not successful. Touchpad with NumberPad not found. Check whether your touchpad has integrated NumberPad (e.g. on product websites) and then eventually create an issue here https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/new/choose."
-        exit 1
+        echo "The detection was not successful. Touchpad with NumberPad not found. Check whether your touchpad has integrated NumberPad (e.g. on product websites) and in case it has then eventually create an issue here https://github.com/asus-linux-drivers/asus-numberpad-driver/issues/new/choose."
+        
+        read -p "Do you want try to continue anyway? [y/N]: " CONTINUE
+
+        if [[ ! "$CONTINUE" =~ ^[Yy]$ ]]; then
+            exit 1
+        fi
     fi
 else
     echo "The i2cdetect or i2ctransfer tool not found to proceed initial test whether any i2c device react like NumberPad"
